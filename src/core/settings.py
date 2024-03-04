21ee0c0fef39ef85f75bcbe6c4a05ebe8fe01778
MirjahonMirsaidov <mirjahonac021@gmail.com>
2024-03-04 23:11:20 +0500

    redis support

--- src/core/settings.py
+++ src/core/settings.py
@@ -158,7 +158,7 @@ REDIS_HOST = env('REDIS_HOST', str)
 CACHES = {
     "default": {
         "BACKEND": "django.core.cache.backends.redis.RedisCache",
-        "LOCATION": "redis://127.0.0.1:6379",
+        "LOCATION": REDIS_HOST,
     }
 }
 
